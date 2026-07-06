9d11b698f9eb951e3bee087afb2d9c479221d766
Eric <F113118104@nkust.edu.tw>
2026-07-06 17:03:49 +0800

    add new 初階IOT

--- Arduino 初階/Arduino-Labs-nkust-v5-master/03-2_Serial_Read/03-2_Serial_Read.ino
+++ Arduino 初階/Arduino-Labs-nkust-v5-master/03-2_Serial_Read/03-2_Serial_Read.ino
@@ -1,40 +1,61 @@
 #include <string.h>
 
+bool blinking = false;          // 是否為閃爍模式
+unsigned long lastBlink = 0;    // 上次切換時間
+bool ledState = LOW;            // LED目前狀態
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  // opens serial port, set data rate to 9600 bps
+  pinMode(3, OUTPUT);
+
   Serial.begin(9600);
-  while(!Serial); // wait until serial port is ready
-  Serial.println("Program Start!!");      
+  while (!Serial);
+
+  Serial.println("Program Start!!");
 }
 
 void loop() {
-  static char buf[200];
-  static int  length = 0;
-  int incoming_byte; 
 
-  // If there is any data incoming, read a byte and put it on string buffer
-  if(Serial.available() > 0) { // check if there is incoming data
-    incoming_byte = Serial.read();  // read the incoming byte
-    buf[length++] = incoming_byte;  // put the incoming byte to string buffer
-    if(incoming_byte == '\n') { // check if LF is incoming
-      // append '\0' to buf[] as the end of the string
-      buf[length++] = '\0';
-      // do job
-      if(strcmp(buf, "on\n")==0) { // check if the incoming string is "on"
-        digitalWrite(LED_BUILTIN, HIGH);
+  // ===== LED 閃爍功能 =====
+  if (blinking) {
+    if (millis() - lastBlink >= 100) {   // 每 500 ms 切換一次
+      lastBlink = millis();
+      ledState = !ledState;
+      digitalWrite(3, ledState);
+    }
+  }
+
+  static char buf[200];
+  static int length = 0;
+  int incoming_byte;
+
+  if (Serial.available() > 0) {
+    incoming_byte = Serial.read();
+    buf[length++] = incoming_byte;
+
+    if (incoming_byte == '\n') {
+      buf[length] = '\0';
+
+      if (strcmp(buf, "on\n") == 0) {
+        blinking = false;                 // 關閉閃爍模式
+        digitalWrite(3, HIGH);
         Serial.println("LED is on.");
       }
-      else if(strcmp(buf, "off\n")==0) { // check if the incoming string is "on"
-        digitalWrite(LED_BUILTIN, LOW);       
+      else if (strcmp(buf, "off\n") == 0) {
+        blinking = false;                 // 關閉閃爍模式
+        digitalWrite(3, LOW);
         Serial.println("LED is off.");
       }
-      else { // Otherwise, it is unknown command
+      else if (strcmp(buf, "shining\n") == 0) {
+        blinking = true;                  // 開啟閃爍模式
+        lastBlink = millis();
+        Serial.println("LED is shining.");
+      }
+      else {
         Serial.print("Unknown command: ");
         Serial.println(buf);
       }
-      length = 0; // reset the length for receiving next string
+
+      length = 0;
     }
   }
 }
